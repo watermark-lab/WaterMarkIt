77dbcb06396af4e7c462b1368b83d8d4e3d8d806
OlegCheban <oleg.cheban1989@gmail.com>
2024-10-14 23:21:12 +0300

    code refactoring

---
+++ src/main/java/com/markit/exceptions/AsyncWatermarkPdfException.kt
@@ -0,0 +1,7 @@
+package com.markit.exceptions
+
+/**
+ * @author Oleg Cheban
+ * @since 1.0
+ */
+class AsyncWatermarkPdfException(cause: Throwable) : RuntimeException(cause)
---
+++ src/main/java/com/markit/exceptions/WatermarkingException.kt
@@ -0,0 +1,7 @@
+package com.markit.exceptions
+
+/**
+ * @author Oleg Cheban
+ * @since 1.0
+ */
+class WatermarkingException(message: String, cause: Throwable) : RuntimeException(message, cause)
