78adc2a2b7e047cbe98f832758bce7cba4b0c821
OlegCheban <oleg.cheban1989@gmail.com>
2025-01-15 13:29:06 +0300

    a new test

--- src/test/java/com/markit/PdfPortraitPageOrientationImageBasedWatermarkTest.kt
+++ src/test/java/com/markit/PdfPortraitPageOrientationImageBasedWatermarkTest.kt
@@ -2,15 +2,19 @@ package com.markit
 
 import com.markit.api.WatermarkPosition
 import com.markit.api.WatermarkService
+import com.markit.api.WatermarkingMethod
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.pdmodel.PDPage
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.awt.Color
 import java.io.File
 import java.io.IOException
 import java.io.InputStream
 import java.nio.file.Files
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.concurrent.Executors
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
@@ -42,9 +46,25 @@ class PdfPortraitPageOrientationImageBasedWatermarkTest {
                 )
         )
             .watermarkPDF(document)
-            .withImage(readFileFromClasspathAsBytes("logo.png")).size(25)
-            .position(WatermarkPosition.TILED)
-            .opacity(0.1f)
+                .withImage(readFileFromClasspathAsBytes("logo.png"))
+                    .position(WatermarkPosition.CENTER)
+                    .opacity(0.2f)
+            .and()
+                .withText("WaterMarkIt")
+                    .addTrademark()
+                    .color(Color.BLUE)
+                    .watermark()
+                        .size(110)
+                        .position(WatermarkPosition.TILED)
+                            .adjust(35, 0)
+                        .opacity(0.1f)
+                        .rotation(25)
+            .and()
+                .withText(LocalDateTime.now().toString())
+                    .watermark()
+                        .position(WatermarkPosition.TOP_RIGHT)
+                            .adjust(0, -30)
+                        .size(50)
             .apply()
 
         // Then
