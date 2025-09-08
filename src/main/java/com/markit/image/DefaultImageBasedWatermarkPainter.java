a4f5af4106893236c1b0b978bbe024f765703479
OlegCheban <oleg.cheban1989@gmail.com>
2025-09-08 10:49:39 +0300

    code refactoring

---
+++ src/main/java/com/markit/image/DefaultImageBasedWatermarkPainter.java
@@ -0,0 +1,63 @@
+package com.markit.image;
+
+import com.markit.api.WatermarkAttributes;
+import com.markit.api.positioning.WatermarkPositionCoordinates;
+
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+/**
+ * Default implementation of {@link ImageBasedWatermarkPainter}.
+ *
+ * @author Oleg Cheban
+ * @since 1.3.5
+ */
+public class DefaultImageBasedWatermarkPainter implements ImageBasedWatermarkPainter {
+
+    @Override
+    public int getPriority() {
+        return DEFAULT_PRIORITY;
+    }
+
+    @Override
+    public void draw(Graphics2D g2d, WatermarkAttributes attr) {
+        BufferedImage watermarkImage = attr.getImage().get();
+        var alphaChannel = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) (attr.getOpacity() / 100.0));
+        configureGraphics(g2d, alphaChannel);
+        int watermarkWidth = (int) (watermarkImage.getWidth() * (attr.getSize() / 100.0));
+        int watermarkHeight = (int) (watermarkImage.getHeight() * (attr.getSize() / 100.0));
+        var coordinates = WatermarkPositioner.defineXY(attr, attr.getImageWidth(), attr.getImageHeight(), watermarkWidth, watermarkHeight);
+        coordinates.forEach(v -> drawWatermark(g2d, watermarkImage, v, watermarkWidth, watermarkHeight, attr.getRotationDegrees()));
+    }
+
+    private void configureGraphics(Graphics2D g2d, AlphaComposite alphaChannel) {
+        g2d.setComposite(alphaChannel);
+    }
+
+    private void drawWatermark(Graphics2D g2d, BufferedImage watermarkImage, WatermarkPositionCoordinates.Coordinates c, int width, int height, int rotation) {
+        applyWithOptionalRotation(g2d, rotation, c, width, height, () -> {
+            g2d.drawImage(watermarkImage, c.getX(), c.getY(), width, height, null);
+        });
+    }
+
+    private void applyWithOptionalRotation(Graphics2D g2d, int rotation, WatermarkPositionCoordinates.Coordinates c, int width, int height, Runnable drawAction) {
+        var originalTransform = g2d.getTransform();
+        if (rotation != 0) {
+            applyRotation(g2d, rotation, c, width, height);
+        }
+        drawAction.run();
+
+        if (rotation != 0) {
+            g2d.setTransform(originalTransform);
+        }
+    }
+
+    private void applyRotation(Graphics2D g2d, int rotation, WatermarkPositionCoordinates.Coordinates c, int width, int height) {
+        double centerX = c.getX() + width / 2.0;
+        double centerY = c.getY() + height / 2.0;
+        g2d.rotate(-Math.toRadians(rotation), centerX, centerY);
+    }
+}
+
+
--- src/main/java/com/markit/image/TextBasedWatermarkPainter.java
+++ src/main/java/com/markit/image/TextBasedWatermarkPainter.java
@@ -1,84 +1,17 @@
 package com.markit.image;
 
 import com.markit.api.WatermarkAttributes;
-import com.markit.api.positioning.WatermarkPositionCoordinates;
+import com.markit.servicelocator.Prioritizable;
 
-import java.awt.*;
-import java.awt.font.FontRenderContext;
-import java.awt.font.TextLayout;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
 
 /**
+ * The interface for text-based watermark painting
+ *
  * @author Oleg Cheban
- * @since 1.0
+ * @since 1.3.5
  */
-public class TextBasedWatermarkPainter {
+public interface TextBasedWatermarkPainter extends Prioritizable {
 
-    public void draw(Graphics2D g2d, BufferedImage image, WatermarkAttributes attr, WatermarkPositioner positioner) {
-        var alphaChannel = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) (attr.getOpacity() / 100.0));
-        var fontSize = calculateFontSize(attr.getSize(), image.getWidth(), image.getHeight());
-        var fontStyle = attr.isBold() ? Font.BOLD : Font.PLAIN;
-        var font = new Font(attr.getFont().getAwtFontName(), fontStyle, fontSize);
-        configureGraphics(g2d, alphaChannel, attr.getColor(), font);
-        FontRenderContext frc = g2d.getFontRenderContext();
-        TextLayout watermarkLayout = new TextLayout(attr.getText(), font, frc);
-        Rectangle2D rect = watermarkLayout.getBounds();
-
-        var coordinates = positioner.defineXY(attr, image.getWidth(), image.getHeight(), (int) rect.getWidth(), (int) rect.getHeight());
-        coordinates.forEach(v -> drawWatermark(g2d, watermarkLayout, attr, rect, v, font, fontSize));
-    }
-
-    private int calculateFontSize(int textSize, int imageWidth, int imageHeight) {
-        if (textSize > 0) return textSize;
-        return Math.min(imageWidth, imageHeight) / 10;
-    }
-
-    private void configureGraphics(Graphics2D g2d, AlphaComposite alphaChannel, Color color, Font font) {
-        g2d.setComposite(alphaChannel);
-        g2d.setColor(color);
-        g2d.setFont(font);
-    }
-
-    private void drawWatermark(Graphics2D g2d, TextLayout watermarkLayout, WatermarkAttributes attr, Rectangle2D rect, WatermarkPositionCoordinates.Coordinates c, Font baseFont, int baseFontSize) {
-        applyWithOptionalRotation(g2d, attr.getRotationDegrees(), c, rect, () -> {
-            watermarkLayout.draw(g2d, c.getX(), c.getY());
-
-            if (attr.getTrademark()) {
-                drawTrademark(g2d, baseFont, baseFontSize, rect, c);
-            }
-        });
-    }
-
-    private void applyWithOptionalRotation(Graphics2D g2d, int rotation, WatermarkPositionCoordinates.Coordinates c, Rectangle2D rect, Runnable drawAction) {
-        var originalTransform = g2d.getTransform();
-        if (rotation != 0) {
-            applyRotation(g2d, rotation, c, rect);
-        }
-        drawAction.run();
-
-        if (rotation != 0) {
-            /**
-             * Restores the original transformation matrix of the Graphics2D object.
-             * This is crucial to ensure that subsequent drawing operations (in case of TILED position) are not affected
-             * by the transformations (e.g., rotation) applied to the current watermark.
-             * Without this reset, transformations would accumulate, leading to incorrect
-             * placement or rendering of other elements.
-             */
-            g2d.setTransform(originalTransform);
-        }
-    }
-
-    private void applyRotation(Graphics2D g2d, int rotation, WatermarkPositionCoordinates.Coordinates c, Rectangle2D rect) {
-        double centerX = c.getX() + rect.getWidth() / 2;
-        double centerY = c.getY() + rect.getHeight() / 2;
-        g2d.rotate(-Math.toRadians(rotation), centerX, centerY);
-    }
-
-    private void drawTrademark(Graphics2D g2d, Font baseFont, int baseFontSize, Rectangle2D rect, WatermarkPositionCoordinates.Coordinates c) {
-        FontRenderContext frc = g2d.getFontRenderContext();
-        Font smallFont = baseFont.deriveFont((float) baseFontSize / 2);
-        TextLayout trademarkLayout = new TextLayout("®", smallFont, frc);
-        trademarkLayout.draw(g2d, (float) (c.getX() + rect.getWidth()) + 5, c.getY() - (baseFontSize / 2f));
-    }
+    void draw(Graphics2D g2d, WatermarkAttributes attr);
 }
