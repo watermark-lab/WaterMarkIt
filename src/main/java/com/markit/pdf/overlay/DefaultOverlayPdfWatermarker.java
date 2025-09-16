eae61e169fe70b26b26448a163a687a59f6fc2a4
OlegCheban <oleg.cheban1989@gmail.com>
2025-09-16 18:25:36 +0300

    code refactoring

--- src/main/java/com/markit/pdf/overlay/DefaultOverlayPdfWatermarker.java
+++ src/main/java/com/markit/pdf/overlay/DefaultOverlayPdfWatermarker.java
@@ -1,8 +1,6 @@
 package com.markit.pdf.overlay;
 
 import com.markit.api.WatermarkAttributes;
-import com.markit.pdf.overlay.font.DefaultFontProvider;
-import com.markit.pdf.overlay.font.FontProvider;
 import com.markit.servicelocator.ServiceFactory;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
@@ -22,13 +20,6 @@ public class DefaultOverlayPdfWatermarker implements OverlayPdfWatermarker {
 
     @Override
     public void watermark(PDDocument document, int pageIndex, List<WatermarkAttributes> attrs) throws IOException {
-        var imageBasedOverlayWatermarker = (ImageBasedOverlayWatermarker) ServiceFactory.getInstance()
-                .getService(ImageBasedOverlayWatermarker.class);
-        var textBasedOverlayWatermarker = (TextBasedOverlayWatermarker) ServiceFactory.getInstance()
-                .getService(TextBasedOverlayWatermarker.class);
-        var graphicsStateManager = (GraphicsStateManager) ServiceFactory.getInstance()
-                .getService(GraphicsStateManager.class);
-
         var page = document.getPage(pageIndex);
 
         try (PDPageContentStream contentStream =
@@ -36,13 +27,19 @@ public class DefaultOverlayPdfWatermarker implements OverlayPdfWatermarker {
 
             attrs.forEach(attr -> {
                 try {
+                    var graphicsStateManager = (GraphicsStateManager) ServiceFactory.getInstance()
+                            .getService(GraphicsStateManager.class);
                     var opacityState = graphicsStateManager.createOpacityState(attr.getOpacity());
                     contentStream.setGraphicsStateParameters(opacityState);
 
                     if (attr.getImage().isPresent()){
                         var image = LosslessFactory.createFromImage(document, attr.getImage().get());
+                        var imageBasedOverlayWatermarker = (ImageBasedOverlayWatermarker) ServiceFactory.getInstance()
+                                .getService(ImageBasedOverlayWatermarker.class);
                         imageBasedOverlayWatermarker.overlay(contentStream, image, page.getMediaBox(), attr);
                     } else {
+                        var textBasedOverlayWatermarker = (TextBasedOverlayWatermarker) ServiceFactory.getInstance()
+                                .getService(TextBasedOverlayWatermarker.class);
                         textBasedOverlayWatermarker.overlay(document, contentStream, page.getMediaBox(), attr);
                     }
                 } catch (IOException e) {
