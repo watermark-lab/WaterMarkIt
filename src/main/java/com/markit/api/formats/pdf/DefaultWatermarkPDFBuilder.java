db7d1871078e1a0efba001dced2ed798693f7530
OlegCheban <oleg.cheban1989@gmail.com>
2025-09-17 16:11:53 +0300

    Removed closing a document. It's the document owner's duty.

--- src/main/java/com/markit/api/formats/pdf/DefaultWatermarkPDFBuilder.java
+++ src/main/java/com/markit/api/formats/pdf/DefaultWatermarkPDFBuilder.java
@@ -2,13 +2,10 @@ package com.markit.api.formats.pdf;
 
 import com.markit.api.builders.DefaultWatermarkBuilder;
 import com.markit.api.WatermarkingMethod;
-import com.markit.exceptions.ClosePDFDocumentException;
 import com.markit.pdf.DefaultWatermarkPdfService;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import com.markit.api.formats.pdf.WatermarkPDFService.*;
-import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
 import java.util.concurrent.Executor;
 import java.util.function.Predicate;
 
@@ -20,11 +17,8 @@ public final class DefaultWatermarkPDFBuilder
         extends DefaultWatermarkBuilder<WatermarkPDFService, WatermarkPDFBuilder>
         implements WatermarkPDFService, WatermarkPDFBuilder {
 
-    private final PDDocument document;
-
     public DefaultWatermarkPDFBuilder(PDDocument pdfDoc, Executor executor) {
         super(watermarks -> new DefaultWatermarkPdfService(executor).watermark(pdfDoc, watermarks));
-        this.document = pdfDoc;
     }
 
     @Override
@@ -49,21 +43,5 @@ public final class DefaultWatermarkPDFBuilder
     public WatermarkPDFBuilder pageFilter(Predicate<Integer> predicate) {
         getWatermark().setPagePredicate(predicate);
         return this;
-    }
-
-    @NotNull
-    @Override
-    public byte[] apply() {
-        var res = super.apply();
-        closeDocument();
-        return res;
-    }
-
-    private void closeDocument() {
-        try {
-            this.document.close();
-        } catch (IOException e) {
-            throw new ClosePDFDocumentException("Failed to close the document", e);
-        }
     }
 }
